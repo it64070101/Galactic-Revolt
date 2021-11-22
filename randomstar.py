eb9cf1f47567806c6fd0ca6074d81b1d540a3f3a
kalamplee <64070101@kmitl.ac.th>
2021-11-22 13:34:19 +0700

    Update randomstar.py

--- randomstar.py
+++ randomstar.py
@@ -9,9 +9,21 @@ def random_star_name():
     #alph = alph.upper()
     #num = " %d.%03d" %(random.randrange(10), random.randrange(1000))+random.choice(string.ascii_letters)
     #return alph+num
-    name = ["numa", "preva", "kelso", "lola", "kelli", "kabo", "sere", "siz", "lak", "sof", "nod", "li", "iru", "ka"]
+    name = ['kim', 'su', 'pan', 'kua', 'nai',
+    'ni', 'kaun', 'niu', 'naum', 'mii',
+    'saa', 'tum','mim', 'sim', 'kuu',
+    'sim', 'muu', 'tin', 'tu', 'ka']
+    
     random.shuffle(name)
     pre = (name.pop()+name.pop()).title()
     suf = "-%02d%s" %(random.randrange(100), random.choice(string.ascii_letters))
-    return pre+suf
+    return pre+suf.lower()
 print(random_star_name())
+def tolist():
+    biglist = []
+    while True:
+        x = input()
+        if x == "END":
+            break
+        biglist.append(x)
+    print(biglist)
